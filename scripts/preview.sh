db48db4e1951ef26dada83dbc635888000523134
cpicoto <cpicoto@users.noreply.github.com>
2025-08-10 18:38:24 -0700

    Fix ffplay command syntax in preview script

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -12,4 +12,4 @@ FILTER="movie=${PREVIEW_UDP_URL},setpts=PTS-STARTPTS,scale=${SCREEN_W}/2:${SCREE
         [loc][rem]hstack=inputs=2, pad=${SCREEN_W}:${SCREEN_H}:(ow-iw)/2:(oh-ih)/2:color=black, \
         drawtext=fontfile=${OVERLAY_FONT}:textfile=/run/rtmp-status.txt:reload=1:x=20:y=H-th-20:box=1:boxborderw=10:boxcolor=black@0.5"
 
-exec ffplay -loglevel error -fflags nobuffer -flags low_delay -fast -probesize 32 -analyzeduration 0 -autoexit 0 -fs -f lavfi -i "$FILTER"
+exec ffplay -loglevel error -fflags nobuffer -flags low_delay -fast -probesize 32 -analyzeduration 0 -autoexit 0 -fs -f lavfi "$FILTER"
